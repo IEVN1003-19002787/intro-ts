97a9c40d9811dec96d6312489fde8be86b7e6968
Antonio Ponce <19002468@alumnos.utleon.edu.mx>
2024-09-06 18:48:41 -0600

    Typescript

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,23 @@
+console.log('Hola mundo')
+
+var num1 = 38
+let num2 = 29
+const num3 = 10
+
+
+
+let sum = num1 + num2 + num3;
+
+//Tipos de datos
+let nombre: String
+let num: number
+let activo: boolean
+
+nombre = 'Mario'
+num = 32
+activo = true
+
+let matricula:number|string|boolean
+matricula=1234567
+matricula='A232B'
+console.log('El numero es', sum);
---
+++ src/temas-ts/02-interfaz.ts
@@ -0,0 +1,13 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    email:string;
+    calificacion?:number;
+}
+
+const alumno:Alumno = {
+
+    nombre: 'Mario',
+    edad: 23,
+    email: 'mario@gmail.com',
+}
