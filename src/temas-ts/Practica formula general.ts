b8c0639254af0fc799d81dc54d4b066a8564f46f
IEVN1003-19002787 <carlos.quirogaprado@gmail.com>
2024-09-11 19:40:23 -0600

    tercer commit 11/09//24

--- src/temas-ts/Practica formula general.ts
+++ src/temas-ts/Practica formula general.ts
@@ -1,4 +1,30 @@
-class Area{
+class FormGeneral{
+    a:number;
+    b:number;
+    c:number;
+    constructor(a:number,b:number,c:number){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    calcular(): { x1: number, x2: number } {
+        let discriminante = Math.pow(this.b, 2) - 4 * this.a * this.c;
+        let x1 = (-this.b + Math.sqrt(discriminante)) / (2 * this.a);
+        let x2 = (-this.b - Math.sqrt(discriminante)) / (2 * this.a);
+        return { x1, x2 };
+    }
+    imprimir() {
+        
+            const { x1, x2 } = this.calcular();
+            console.log(Los resultados son: ${x1} y ${x2});
+        
+    }
+}
+
+const ecuacion=new Ecuacion(1,6,8);
+ecuacion.imprimir();
+
+/*class Area{
     protected num1:number;
     protected num2:number;
     constructor(num1:number, num2:number){
@@ -6,4 +32,4 @@ class Area{
         this.num2= num2;
     }
 }
-const area=new Area(55, 44);
+const area=new Area(55, 44);*/
